b9b2841249cb7f585ed6f4eb854a66cd8c82e551
Igor Dianov <igor@dianov.com>
2019-01-22 22:42:39 -0800

    fix: implement Notifications Steps

--- activiti-cloud-acceptance-tests-core/src/main/java/org/activiti/cloud/acc/core/steps/notifications/NotificationsSteps.java
+++ activiti-cloud-acceptance-tests-core/src/main/java/org/activiti/cloud/acc/core/steps/notifications/NotificationsSteps.java
@@ -4,88 +4,105 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Duration;
 
-import net.thucydides.core.annotations.Step;
+import org.activiti.cloud.acc.core.config.RuntimeTestsConfigurationProperties;
 import org.activiti.cloud.acc.core.rest.RuntimeDirtyContextHandler;
 import org.activiti.cloud.acc.core.rest.feign.EnableRuntimeFeignContext;
-import org.activiti.cloud.acc.core.services.notifications.NotificationsService;
+import org.activiti.cloud.acc.core.services.runtime.ProcessRuntimeService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import net.thucydides.core.annotations.Step;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.ReplayProcessor;
+import reactor.netty.NettyPipeline;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.http.client.HttpClient.WebsocketSender;
 import reactor.test.StepVerifier;
 
 @EnableRuntimeFeignContext
 public class NotificationsSteps {
     
-    private static final String WS_GRAPHQL_URI = "/ws/graphql";
     private static final String GRAPHQL_WS = "graphql-ws";
-    private static final String HRUSER = "hruser";
     private static final String AUTHORIZATION = "Authorization";
-    private static final String TESTADMIN = "testadmin";
-    private static final String TASK_NAME = "task1";
-    private static final String GRPAPHQL_URL = "/graphql";
     private static final Duration TIMEOUT = Duration.ofMillis(20000);
     
-
     @Autowired
     private RuntimeDirtyContextHandler dirtyContextHandler;
+    
+    @Autowired
+    private RuntimeTestsConfigurationProperties properties;
 
     @Autowired
-    private NotificationsService processService;
+    private ProcessRuntimeService processRuntimeService;
 
     @Step
     public void checkServicesHealth() {
-        assertThat(processService.isServiceUp()).isTrue();
-    }
-
-    @Step
-    public ReplayProcessor<String> subscribe(String host,String port,String auth) {
-        ReplayProcessor<String> data = ReplayProcessor.create();
-        return data;
-        
-//        WebsocketSender client = HttpClient.create()
-//                .baseUrl("ws://"+host+":" + port)
-//                .wiretap(true)
-//                .headers(h -> h.add(AUTHORIZATION, auth))
-//                .websocket(GRAPHQL_WS)
-//                .uri(WS_GRAPHQL_URI);
-//        
-//        String startMessage = "{\"id\":\"1\",\"type\":\"start\",\"payload\":{\"query\":\"subscription {\\n engineEvents(appName: \\\"default-app\\\") {\\n appName\\n serviceName\\n }\\n}\",\"variables\":null}}";
-//        
-//        // start subscription
-//        client.handle((i, o) -> {
-//                o.options(NettyPipeline.SendOptions::flushOnEach)
-//                .sendString(Mono.just(startMessage))
-//                .then()
-//                .log("start")
-//                .subscribe();
-//    
-//                    return i.receive()
-//                        .asString()
-//                        .log("data")
-//                        .take(2)
-//                        .subscribeWith(data);
-//                }) // stop subscription
-//                .collectList()
-//                .subscribe();
-//
-//       // start process instance
-//       // complete
-//       // expect complete 
-//                
-//        return data;
+        assertThat(processRuntimeService.isServiceUp()).isTrue();
     }
     
     @Step
-    public void verifySubscribe(ReplayProcessor<String> data) {
+    public ReplayProcessor<String> subscribe(String accessToken) throws InterruptedException {
+        ReplayProcessor<String> data = ReplayProcessor.create();
         
-        String ackMessage = "{\"payload\":{},\"id\":null,\"type\":\"connection_ack\"}";
-        String kaMessage = "{\"payload\":{},\"id\":null,\"type\":\"ka\"}";
-        String dataMessage = "{\"payload\":{\"data\":{\"engineEvents\":{\"appName\":\"default-app\",\"serviceName\":\"rb-my-app\"}}},\"id\":\"1\",\"type\":\"data\"}";
-        String completeMessage = "{\"payload\":{},\"id\":\"1\",\"type\":\"complete\"}";
+        WebsocketSender client = HttpClient.create()
+                .wiretap(true)
+                .headers(h -> h.add(AUTHORIZATION, "Bearer " + accessToken))
+                .websocket(GRAPHQL_WS)
+                .uri(properties.getGraphqlWsUrl());
+        
+        String startMessage = "{\"id\":\"1\",\"type\":\"start\",\"payload\":{\"query\":\""
+        		+ "subscription {" + 
+        		"  engineEvents(serviceName: \\\"rb-my-app\\\", processDefinitionKey: \\\"ConnectorProcess\\\" ) {" + 
+        		"    appName" + 
+        		"    serviceName" + 
+        		"    processDefinitionKey" + 
+        		"    PROCESS_STARTED {" + 
+        		"      entity {" + 
+        		"        status" + 
+        		"      }" + 
+        		"    }" + 
+        		"    PROCESS_COMPLETED {" + 
+        		"      entity {" + 
+        		"        status" + 
+        		"      }" + 
+        		"    }" + 
+        		"  }" + 
+        		"}"
+        		+ "\",\"variables\":null}}";
+        
+        // handle start subscription
+        client.handle((i, o) -> {
+                o.options(NettyPipeline.SendOptions::flushOnEach)
+                .sendString(Mono.just(startMessage))
+                .then()
+                .log("start")
+                .subscribe();
+    
+                    return i.receive()
+                        .asString()
+                        .log("data")
+                        .take(2) // stop subscription after 2 data messages received
+                        .subscribeWith(data);
+                }) 
+                .collectList()
+                .subscribe();
 
-        StepVerifier.create(data)
-        .expectNext(dataMessage)
-        .expectNext(dataMessage)
-        .expectComplete()
-        .verify(TIMEOUT);
+        // Let's wait for subscribe 
+        Thread.sleep(1000);
+        
+        return data;
     }
+    
+    @Step
+    public void verifyData(ReplayProcessor<String> data) {
+        
+        String startProcessMessage = "{\"payload\":{\"data\":{\"engineEvents\":{\"appName\":\"default-app\",\"serviceName\":\"rb-my-app\",\"processDefinitionKey\":\"ConnectorProcess\",\"PROCESS_STARTED\":[{\"entity\":{\"status\":\"RUNNING\"}}],\"PROCESS_COMPLETED\":null}}},\"id\":\"1\",\"type\":\"data\"}";
+        String completeProcessMessage = "{\"payload\":{\"data\":{\"engineEvents\":{\"appName\":\"default-app\",\"serviceName\":\"rb-my-app\",\"processDefinitionKey\":\"ConnectorProcess\",\"PROCESS_STARTED\":null,\"PROCESS_COMPLETED\":[{\"entity\":{\"status\":\"COMPLETED\"}}]}}},\"id\":\"1\",\"type\":\"data\"}";
+
+		StepVerifier.create(data)
+		            .expectNext(startProcessMessage)
+		            .expectNext(completeProcessMessage)
+		            .expectComplete()
+		            .verify(TIMEOUT);
+    }
+    
 }
