783746a88cdbc795eed626be7527bad2ae434f1d
CTI777 <illia.goncharov@googlemail.com>
2019-01-29 21:33:16 +0100

    Step for admin deleteProcessInstance added

--- activiti-cloud-acceptance-tests-core/src/main/java/org/activiti/cloud/acc/core/services/runtime/admin/ProcessRuntimeAdminService.java
+++ activiti-cloud-acceptance-tests-core/src/main/java/org/activiti/cloud/acc/core/services/runtime/admin/ProcessRuntimeAdminService.java
@@ -1,9 +1,10 @@
 package org.activiti.cloud.acc.core.services.runtime.admin;
 
 import feign.Headers;
+import feign.Param;
 import feign.RequestLine;
-import org.activiti.cloud.api.process.model.CloudProcessInstance;
 import org.activiti.cloud.acc.shared.service.BaseService;
+import org.activiti.cloud.api.process.model.CloudProcessInstance;
 import org.springframework.hateoas.PagedResources;
 
 public interface ProcessRuntimeAdminService extends BaseService {
@@ -11,5 +12,9 @@ public interface ProcessRuntimeAdminService extends BaseService {
     @RequestLine("GET /admin/v1/process-instances")
     @Headers("Content-Type: application/json")
     PagedResources<CloudProcessInstance> getProcessInstances();
+    
+    @RequestLine("DELETE /admin/v1/process-instances/{id}")
+    @Headers("Content-Type: application/json")
+    void deleteProcess(@Param("id") String id);
 
 }
